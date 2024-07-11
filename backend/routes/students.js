6b37b91dbc776992bb8542934c78ad0bcef5b6ea
Raymond Oo <raymondoo2006@gmail.com>
2024-07-12 00:02:41 +0630

    fixing file structure

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const verifySession = require('../middleware/verifySession');
+const verifySession = require('../middleware/verifysession');
 
 // Get all students
 router.get('/', verifySession, async (req, res) => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const verifySession = require('../middleware/verifySession');
+const verifySession = require('../middleware/verifysession');
 
 router.get('/me', verifySession, (req, res) => {
     res.send(req.user);
