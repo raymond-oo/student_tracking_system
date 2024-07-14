0b0f49947796b6beba66de60f534e7df703ac9d9
Raymond Oo <raymondoo2006@gmail.com>
2024-07-14 16:32:36 +0630

    removed libraries + added placeholders to update students

--- src/Students/UpdateStudent.js
+++ src/Students/UpdateStudent.js
@@ -73,12 +73,12 @@ const UpdateStudent = () => {
                 <h2>Update Student</h2>
                 {error && <p className="error">{error}</p>}
                 <form onSubmit={handleSubmit}>
-                    <input type="text" name="first_name" value={student.first_name} onChange={handleChange} required />
-                    <input type="text" name="last_name" value={student.last_name} onChange={handleChange} required />
-                    <input type="email" name="email" value={student.email} onChange={handleChange} required />
-                    <input type="text" name="grade" value={student.grade} onChange={handleChange} required />
-                    <input type="text" name="list_of_trained_tools" value={student.list_of_trained_tools.join(',')} onChange={handleChange} />
-                    <input type="file" name="profile_picture" onChange={handleChange} accept="image/*" />
+                    <input placeholder="First Name" type="text" name="first_name" value={student.first_name} onChange={handleChange} required />
+                    <input placeholder="Last Name" type="text" name="last_name" value={student.last_name} onChange={handleChange} required />
+                    <input placeholder="example@isyedu.org" type="email" name="email" value={student.email} onChange={handleChange} required />
+                    <input placeholder="Grade" type="text" name="grade" value={student.grade} onChange={handleChange} required />
+                    <input placeholder="List of Trained Tools" type="text" name="list_of_trained_tools" value={student.list_of_trained_tools.join(',')} onChange={handleChange} />
+                    <input placeholder="Profile Picture" type="file" name="profile_picture" onChange={handleChange} accept="image/*" />
                     <div className="button-group">
                         <button type="submit">Update Student</button>
                         <button type="button" onClick={handleCancel}>Cancel</button>
