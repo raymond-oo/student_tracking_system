e682c9fed60fa02feee317a7c4bcd2475170d956
Raymond Oo <raymondoo2006@gmail.com>
2024-07-24 00:09:56 +0630

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   grade: { type: String },
   is_admin: { type: Boolean, default: false },
-  list_of_trained_tools: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tool' }],
+  list_of_trained_tools: [{ type: Array}],
   profile_image: { type: String },
   session: { type: String },
 });
