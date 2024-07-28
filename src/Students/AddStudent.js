25a5e7ec34d583e07526536f030c6be2838126e0
Raymond Oo <raymondoo2006@gmail.com>
2024-07-29 03:32:39 +0630

    more UI changes

--- src/Students/AddStudent.js
+++ src/Students/AddStudent.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 import Header from '../components/Header';
 import toast from 'react-hot-toast';
 import '../styles/AddStudent.css';
