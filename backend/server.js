a34996fd653c09ae892dd3b31b789d7086e1f713
Raymond Oo <raymondoo2006@gmail.com>
2024-08-08 15:57:41 +0630

    Revert "allow backend to allow requests from frontend"
    This reverts commit a0008e943735daf43c51e96c29f18fe838c292e3.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -10,37 +12,24 @@ const toolsRouter = require('./routes/tools');
 
 const app = express();
 
-// CORS configuration
-const allowedOrigins = [
-    'https://student-training-system-two.vercel.app', // Add your frontend domain here
-    'https://another-allowed-domain.com' // Add other allowed domains if any
-];
-
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Enable to allow cookies with CORS
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
 }));
-
 app.use(bodyParser.json());
+
+app.use('/uploads', express.static('uploads'));
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
+
 
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRouter);
 app.use('/api/protected', protectedRoutes);
 app.use('/api/students', studentsRoute);
 app.use('/api/tools', toolsRouter);
-
-// Serve static files from the 'uploads' directory
-app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
