7c98403523d5592aa7c1a52bf2c06db102d0ea1d
Raymond Oo <raymondoo2006@gmail.com>
2024-08-08 22:53:38 +0630

    Update ToolProfile.js

--- src/Tools/ToolProfile.js
+++ src/Tools/ToolProfile.js
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../components/Header';
 import '../styles/ToolProfile.css';
-import backarrow from '../resources/backarrow.svg';
+import backarrow from '../resources/back-arrow.svg';
 
 const ToolProfile = () => {
     const [tool, setTool] = useState(null);
